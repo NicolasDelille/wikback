8ec12e88f44be6115cdaae99ac30fe1562bd2de1
NicolasDelille <nicolas.delille@gmail.com>
2015-08-19 12:24:56 +0200

    Update style

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -6,7 +6,7 @@
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
-	<link rel="stylesheet" href="/docs/api/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
 <body>
@@ -22,5 +22,6 @@
 		<footer>
 		</footer>
 	</div>
+
 </body>
 </html>
