1ec4cdddc37bfea213fb18f195eacf06eb919ae7
NicolasDelille <nicolas.delille@gmail.com>
2015-08-19 14:59:20 +0200

    Update style

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -4,9 +4,10 @@
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
 
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
 <body>
